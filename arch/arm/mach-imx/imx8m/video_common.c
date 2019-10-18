589c78e2c9b33b7d744b63de55b8bb0182b07935
Davide Ballestrero <davide.ballestrero@finmeccanica.com>
2019-10-18 10:31:29 +0200

    Revert "corretta chiamata senza define per HDMI."
    This reverts commit 443d662f2b116b738aa29854d31ddcb8f148a6d3.

--- arch/arm/mach-imx/imx8m/video_common.c
+++ arch/arm/mach-imx/imx8m/video_common.c
@@ -652,6 +652,7 @@ void *video_hw_init(void)
 {
 	void *fb;
 	int encoding = 1;
+	int ret;
 
 	debug("%s()\n", __func__);
 
@@ -676,13 +677,11 @@ void *video_hw_init(void)
 
 	imx8m_create_color_bar((void *)((uint64_t) fb), &gmode);
 
-#ifdef CONFIG_VIDEO_IMX8_HDMI
-	int ret = imx8_hdmi_enable(encoding, &gmode); /* may change gmode */
+	ret = imx8_hdmi_enable(encoding, &gmode); /* may change gmode */
 	if (ret) {
 		printf("HDMI enable failed!\n");
 		return NULL;
 	}
-#endif
 
 	/* start dccs */
 	imx8m_display_init((uint64_t) fb, encoding, &gmode);
